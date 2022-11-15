6181920ba2d5440a89b41cb850ebaddab00cb133
unanchoi <unusualc@likelion.org>
2022-11-16 02:46:44 +0900

    chore: Add reponseMessage module

---
+++ src/modules/reponseMessage.ts
@@ -0,0 +1,10 @@
+const message = {
+    OK: "성공",
+    BAD_REQUEST: "요청을 잘못 보냈습니다.",
+    PAGE_NOT_FOUND: "요청하신 페이지를 찾을 수 없습니다",
+    NEED_LOGIN: "로그인이 필요합니다.",
+    NO_CONTENT: "데이터가 없습니다.",
+    REPEATED_VALUE: "중복된 데이터입니다.",
+    UNAUTHORIZED: "권한이 없습니다",
+    SERVER_ERROR: "서버 내부 오류"
+}
