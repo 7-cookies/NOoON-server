b2bbfc5e4f3df8835b87938608e65d6b130e4f09
unanchoi <unusualc@likelion.org>
2022-11-23 00:26:54 +0900

    fix: route url 수정

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,11 @@ import userRouter from "./userRouter";
 import placeRouter from "./placeRouter";
 import snowmanRouter from "./snowmanRouter";
 
+
 const router: Router = Router();
 
 router.use("/user", userRouter);
-router.use("./place", placeRouter);
-router.use("./place/:invitationCode/snowman", snowmanRouter);
+router.use("/place", placeRouter);
+router.use("/place/:invitationCode/snowman", snowmanRouter);
 
 export default router;
