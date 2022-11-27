d18503743a68e72a991a21bc38dfe80b59079515
na-yk <ninghao17@naver.com>
2022-11-27 23:31:14 +0900

    feat: Add router for createPlace name 공백 및 길이 20자 이내 여부, background 정수형 여부 검증 auth 임시 주석처리

--- src/routes/placeRouter.ts
+++ src/routes/placeRouter.ts
@@ -1,6 +1,16 @@
 import { Router } from "express";
 import { placeController } from "../controllers";
+import { body } from 'express-validator';
+import auth from "../middlewares/auth";
 
 const router: Router = Router();
+
+router.post('/',
+[body('name').notEmpty(),
+body('name').isLength({min:1, max:20}),
+body('background').isInt()],
+// auth,
+placeController.createPlace);
+
 
 export default router;
