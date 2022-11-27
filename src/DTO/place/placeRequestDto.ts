b8fb73b941e8b99e80ba2b6a17fd8ad1abf47d92
na-yk <ninghao17@naver.com>
2022-11-27 23:28:46 +0900

    feat: Add PlaceCreateRequestDto

--- src/DTO/place/placeRequestDto.ts
+++ src/DTO/place/placeRequestDto.ts
@@ -2,3 +2,9 @@ export interface PlaceRequestDto {
     name: string;
     background: number;
 }
+
+export interface PlaceCreateRequestDto {
+    name: string;
+    background: number;
+    invitationCode: string;
+}
