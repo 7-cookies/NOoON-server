c0d654cecda15d5c8ae225e8bcdb0bc7761f5a60
unanchoi <unusualc@likelion.org>
2022-11-28 13:33:48 +0900

    feat: snowmanRouter 구현

--- src/routes/snowmanRouter.ts
+++ src/routes/snowmanRouter.ts
@@ -1,6 +1,22 @@
 import { Router } from "express";
 import { snowmanController } from "../controllers";
+import auth from "../middlewares/auth";
+import { body } from "express-validator";
 
 const router: Router = Router();
+
+router.post('/',
+body('head').not().notEmpty(),
+body('accessory').notEmpty(),
+body('eye').notEmpty(),
+body('nose').notEmpty(),
+body('mouse').notEmpty(),
+body('arm').notEmpty(),
+body('letter').isString().isLength({max:100}),
+body('creator').isString().isLength({max:10}),
+snowmanController.createSnowman);
+
+router.get('/:snowmanId',
+snowmanController.findSnowman)
 
 export default router;
