92a63b4cb7b187075dc5a6b0506adc9d030ccc77
unanchoi <unusualc@likelion.org>
2022-11-28 15:17:46 +0900

    refactor: snowman 검증 추가

--- src/routes/snowmanRouter.ts
+++ src/routes/snowmanRouter.ts
@@ -6,17 +6,19 @@ import { body } from "express-validator";
 const router: Router = Router();
 
 router.post('/',
-body('head').not().notEmpty(),
-body('accessory').notEmpty(),
-body('eye').notEmpty(),
-body('nose').notEmpty(),
-body('mouse').notEmpty(),
-body('arm').notEmpty(),
-body('letter').isString().isLength({max:100}),
-body('creator').isString().isLength({max:10}),
+body('head').not().notEmpty().isNumeric(),
+body('accessory').notEmpty().isNumeric(),
+body('eye').notEmpty().isNumeric(),
+body('nose').notEmpty().isNumeric(),
+body('mouse').notEmpty().isNumeric(),
+body('arm').notEmpty().isNumeric(),
+body('letter').isString().isLength({min:1, max:100}).withMessage("편지는 최소 1자, 최대 100자입니다."),
+body('creator').isString().isLength({min:1, max:10}).withMessage("만든사람은 최소 1자, 최대 10자입니다."),
+auth,
 snowmanController.createSnowman);
 
 router.get('/:snowmanId',
+auth,
 snowmanController.findSnowman)
 
 export default router;
