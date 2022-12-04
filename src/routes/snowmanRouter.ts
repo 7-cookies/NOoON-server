ac7df71e2ac14a9fc530d56ffd6424bab82d44f3
unanchoi <unusualc@likelion.org>
2022-12-04 17:34:36 +0900

    feat: Add timeLimit middleware

--- src/routes/snowmanRouter.ts
+++ src/routes/snowmanRouter.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { snowmanController } from "../controllers";
 import auth from "../middlewares/auth";
+import { timeLimit } from "../middlewares";
 import { body } from "express-validator";
 
 const router: Router = Router();
@@ -14,11 +15,9 @@ body('mouse').notEmpty().isNumeric(),
 body('arm').notEmpty().isNumeric(),
 body('letter').isString().isLength({min:1, max:100}).withMessage("편지는 최소 1자, 최대 100자입니다."),
 body('creator').isString().isLength({min:1, max:10}).withMessage("만든사람은 최소 1자, 최대 10자입니다."),
-auth,
 snowmanController.createSnowman);
 
-router.get('/:snowmanId',
-auth,
+router.get('/:snowmanId', timeLimit, 
 snowmanController.findSnowman)
 
 export default router;
