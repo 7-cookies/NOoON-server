6e84da6ed12f8088c42e47d847f500597119a4e4
Na Yeongkyeong <102007066+na-yk@users.noreply.github.com>
2022-12-07 20:15:22 +0900

    refactor: Add PlaceGetResponseFinalDto getPlace API의 service 단계에서 가공할 response 형식 DTO 추가

--- src/dto/place/placeResponseDto.ts
+++ src/dto/place/placeResponseDto.ts
@@ -21,3 +21,18 @@ export interface PlaceGetResponseDto {
         snowman_placeTosnowman_place_id?: number;
     };
 }
+export interface PlaceGetResponseFinalDto{
+    name: string;
+    invitation_code: string;
+    snowmans: {
+        id?: number;
+        head?: number;
+        accessory?: number;
+        eye?: number;
+        nose?: number;
+        mouth?: number;
+        arm?: number;
+        creator?: string;
+    }[];
+    count?: number;
+}
