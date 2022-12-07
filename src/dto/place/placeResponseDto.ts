a9dba1c5cc5293113b90ca7de5760bb22bdf2b7e
Na Yeongkyeong <102007066+na-yk@users.noreply.github.com>
2022-12-07 18:05:26 +0900

    refactor: Add PlaceGetResponseDto

--- src/dto/place/placeResponseDto.ts
+++ src/dto/place/placeResponseDto.ts
@@ -3,3 +3,21 @@ export interface PlaceResponseDto {
     background: number;
     invitationCode?: string;
 }
+
+export interface PlaceGetResponseDto {
+    name: string;
+    invitation_code: string;
+    snowman_placeTosnowman_place_id: {
+        id?: number;
+        head?: number;
+        accessory?: number;
+        eye?: number;
+        nose?: number;
+        mouth?: number;
+        arm?: number;
+        creator?: string;
+    }[];
+    _count: {
+        snowman_placeTosnowman_place_id?: number;
+    };
+}
