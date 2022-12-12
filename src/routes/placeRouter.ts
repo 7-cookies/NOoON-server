a793dc1b2a0a61af9bfb1a1ee92f7c84587a9e04
unanchoi <unusualc@likelion.org>
2022-12-13 00:35:03 +0900

    style: PlaceRouter 코드 정렬

--- src/routes/placeRouter.ts
+++ src/routes/placeRouter.ts
@@ -6,14 +6,16 @@ import auth from "../middlewares/auth";
 const router: Router = Router();
 
 router.post('/',
-[body('name').notEmpty(),
-body('name').isLength({min:1, max:20})
-.withMessage('동산 이름은 1~20자 제한입니다.'),
-body('background').isInt()],
-auth,
-placeController.createPlace);
+    [
+        body('name').notEmpty(),
+        body('name').isLength({min:1, max:20}).withMessage('동산 이름은 1~20자 제한입니다.'),
+        body('background').isInt()
+    ],
+    auth,
+    placeController.createPlace
+);
 
-router.get('/:invitationCode/',
+router.get('/:invitationCode',
 placeController.getPlace);
 
 router.get('/:invitationCode/user',
