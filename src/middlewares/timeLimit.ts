fba92c10e41781d020caa4871e51872c76b8d103
unanchoi <unusualc@likelion.org>
2022-12-21 22:47:09 +0900

    style: middleware console.log 삭제

--- src/middlewares/timeLimit.ts
+++ src/middlewares/timeLimit.ts
@@ -7,7 +7,6 @@ const timeLimit = async (req:Request, res:Response, next:NextFunction) => {
     const openTime = new Date('2022-12-25');
 
     const utc = curr.getTime() + (curr.getTimezoneOffset() * 60 * 1000);
-    console.log(utc);
     const openUtc = openTime.getTime() + (openTime.getTimezoneOffset() * 60 * 1000);
 
     const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
@@ -15,10 +14,6 @@ const timeLimit = async (req:Request, res:Response, next:NextFunction) => {
     const krCurr = new Date(utc + (KR_TIME_DIFF));
     
     const krOpenTime = new Date(openUtc + (KR_TIME_DIFF));
-
-    console.log(curr)
-    console.log(krCurr)
-    console.log(krOpenTime)
 
         if (krCurr < krOpenTime) {
             return res.status(sc.FORBIDDEN).send(fail(sc.FORBIDDEN, m.GET_SNOWMAN_TIME_LIMIT))
